37d79b365b137bffa739c0e1a29b1fb1b81aefaa
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-08-24 21:57:34 +0530

    added photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
+import me from "../assets/me.png";
+
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center  px-6 text-center relative">
-        
+    <div className="flex flex-col items-center justify-center  px-6 text-center relative min-h-screen ">
+      <div className="name-sec flex gap-6 items-center">
+        <div className="h-18 w-18 bg-[#FFFFE3] rounded-full relative overflow-hidden">
+          <img className="absolute object-cover w-full " src={me} />
+        </div>
+      </div>
     </div>
   );
 };
